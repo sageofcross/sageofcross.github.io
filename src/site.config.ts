1d49a7b33c752848c220b27b78e43ee997869b21
Aggro <sageofcross@gmail.com>
2026-03-25 19:52:03 +0800

    feat: 調整風格與翻譯英文為繁中

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,29 +3,30 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
-	url: "https://astro-cactus.chriswilliams.dev/",
+	url: "https://sageofcross.github.io/",
 	/*
 		- Used to construct the meta title property found in src/components/BaseHead.astro L:11
 		- The webmanifest name found in astro.config.ts L:42
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "Astro Cactus",
+	title: "Neko in Boots",
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Aggro",
 	// Used as the default description meta property and webmanifest description
-	description: "An opinionated starter theme for Astro",
+	description: "拿來放置各種各樣雜物的空間",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
-	lang: "en-GB",
+	lang: "zh-Hant",
 	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_GB",
+	ogLocale: "zh_Hant",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
-		locale: "en-GB",
+		locale: "zh-Hant-TW",
 		options: {
-			day: "numeric",
-			month: "short",
 			year: "numeric",
+			month: "2-digit",
+			day: "2-digit",
+			hour12: false,
 		},
 	},
 };
@@ -34,19 +35,19 @@ export const siteConfig: SiteConfig = {
 export const menuLinks: { path: string; title: string }[] = [
 	{
 		path: "/",
-		title: "Home",
+		title: "首頁",
 	},
 	{
 		path: "/about/",
-		title: "About",
+		title: "關於",
 	},
 	{
 		path: "/posts/",
-		title: "Blog",
+		title: "文章",
 	},
 	{
 		path: "/notes/",
-		title: "Notes",
+		title: "筆記",
 	},
 ];
 
